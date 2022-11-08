1767737d2afedff680e0a80676cb2a621d7d3816
lakyulakyu <martieszhidan@gmail.com>
2022-11-08 08:12:14 +0700

    modul-1

---
+++ models/admin.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const { FOREIGNKEYS } = require('sequelize/types/query-types');
+module.exports = (sequelize, DataTypes) => {
+  class admin extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+
+      this.hasMany(models.borrow,{
+      foreignKey:'adminID',as:"borrowed",
+      })
+    }
+  }
+  admin.init({
+    name: DataTypes.STRING,
+    contact: DataTypes.STRING,
+    address: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'admin',
+  });
+  return admin;
+};
